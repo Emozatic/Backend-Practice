f676410d6d5d5a82703c19a8e5936c55c7a76309
Lucky Sah <lk4179869@gmail.com>
2026-06-13 21:28:08 +0530

    add: routes, query , params

--- 1/index.js
+++ 1/index.js
@@ -2,6 +2,27 @@ const express= require("express");
 const app= express();
 
 const port= 3000;
+
+
+// app.use((req,res)=>{
+//     res.send("hello world")
+//     console.log("console")
+//     next()      
+// })
+
+app.get("/",(req,res)=>{
+    res.send("home tab");
+})
+
+app.get("/home/:id",(req,res)=>{
+    console.log(req.params.id);
+})
+
+app.get("/home",(req,res)=>{
+    console.log(req.query.q);
+})
+
+
 app.listen(port,()=>{
     console.log("app is listening at 3000");
 })
