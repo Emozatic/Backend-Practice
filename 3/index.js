f5b77be86d52afd038e803a8348ad4bc7f8ebf7a
Lucky Sah <lk4179869@gmail.com>
2026-06-15 20:41:15 +0530

    add: CRUD operations

--- 3/index.js
+++ 3/index.js
@@ -4,6 +4,7 @@ const path=require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"/views"));
+app.use(express.json());
 
 
 // app.get("/profile/:name",(req,res)=>{
@@ -20,19 +21,37 @@ app.set("views", path.join(__dirname,"/views"));
 //     res.send("done");
 // })
 
-app.get("/home",(req,res)=>{
-    let name= "Lucky";
-    res.render("home.ejs", {name})
+// app.get("/home",(req,res)=>{
+//     let name= "Lucky";
+//     res.render("home.ejs", {name})
+// })
+
+// app.get("/data",(req,res)=>{
+//     res.render("home.ejs",{
+//         name:"Lucky",
+//         age:21,
+//         skills:["art", "code", "edit"]
+//     })
+// })
+
+let users=["Lucky", "Rahul"];
+app.get("/home", (req,res)=>{
+    res.send(users);
 })
 
-app.get("/data",(req,res)=>{
-    res.render("home.ejs",{
-        name:"Lucky",
-        age:21,
-        skills:["art", "code", "edit"]
+app.post("/home",(req,res)=>{
+    users.push(req.body.name);
+    console.log(users.body)
+    res.send(users);
+})
+
+app.delete("/home/:name",(req,res)=>{
+    let username= req.params.name;
+    users.filter((users)=>{
+        return users!==username;
     })
+    res.redirect(users)
 })
-
 app.listen(8000,(req,res)=>{
     console.log("app is listening at 8000");
 })
