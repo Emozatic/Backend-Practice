f9dcc83bc8c4a8586afd13db6c6ec856db35303c
Lucky Sah <lk4179869@gmail.com>
2026-06-19 19:16:19 +0530

    \

--- 8/index.js
+++ 8/index.js
@@ -4,13 +4,15 @@ const path= require("path");
 const mongoose= require("mongoose")
 const methodOverride= require("method-override")
 const Listing= require("./listing");
-
+const ejsMate= require("ejs-mate")
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"))
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
 app.use(methodOverride("_method"))
+app.engine("ejs",ejsMate)
+app.use(express.static(path.join(__dirname,"/public")))
 
 main().then((res)=>{
     console.log("connected to mongoose")
