2876005e6b151274554034ffe565e70e21869f49
Lucky Sah <lk4179869@gmail.com>
2026-06-27 18:16:23 +0530

    add: mongoose basics with express

--- 1/index.js
+++ 1/index.js
@@ -9,7 +9,7 @@ async function main(){
 }
 
 main().then(()=>{
-    console.log("connected to mongo");
+    console.log("connected to mongoDB");
 }).catch((err)=>{
     console.log(err);
 })
@@ -20,18 +20,49 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"))
 
 
-app.get("/home",(req,res)=>{
-    res.render("home.ejs");
+const newSchema= new mongoose.Schema({
+    name:String,
+    age:Number,
+});
+let Student= mongoose.model("Student",newSchema);
+
+const stud1= new Student({
+    name:"Lucky",
+    age:20
 })
 
-app.get("/form",(req,res)=>{
-    res.render("form.ejs");
-})
+// stud1.save().then((res)=>{
+//     console.log(res)
+// }).catch((err)=>{
+//     console.log(err)
+// })
 
-app.post("/register",(req,res)=>{
-    res.send("registered")
-    console.log(req.body.username)
-})
+// Student.insertMany([
+//     {
+//         name:"Rocky",
+//         age:18
+//     },
+//     {
+//         name:"Rahul",
+//         age:22
+//     }
+// ]).then((res)=>{
+//     console.log(res)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+// Student.find().then((res)=>{
+//     console.log(res)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+// Student.findByIdAndDelete("6a3fc4a35ec420b0dfd4df68").then((res)=>{
+//     console.log(res)
+// }).catch((err)=>{
+//     console.log(err)
+// })
 
 const port= 8000;
 app.listen(port,()=>{
